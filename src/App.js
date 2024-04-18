1f48149eb5c12cd6b511a9ba26a2a123ed742478
JeremyCanDoIt <jeremy.solomon.chang@gmail.com>
2024-04-18 02:38:29 -0700

    landing page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
-import './App.css'; //Import  CSS file for styling
-
-// Import  image file
-import backgroundImage from "./images/background-asset.png"; // from src folder
+import './App.css';
+import WaveRibbon from './WaveRibbon'; // Import the WaveRibbon component
 
 function App() {
-  // Inline styles to set the background image
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`, // Set the background image
-    backgroundSize: 'contain', // Cover the entire container
-    backgroundRepeat: 'no-repeat', // Do not repeat the background image
-    Height: '100vh', // Set the height to viewport height
-    
-    display: 'flex', // Flex display for centering content
-    justifyContent: 'center', // Center horizontally
-    alignItems: 'center', // Center vertically
-    color: 'white', // Text color
+  const gradientBackground = {
+    background: 'rgb(211,255,253)',
+    background: 'linear-gradient(180deg, rgba(211,255,253,1) 0%, rgba(149,241,237,1) 22%, rgba(91,197,219,1) 51%, rgba(94,169,252,1) 100%)'
   };
 
+  const titleStyle = {
+    top: '5%',
+    left: '5%',
+    position: 'fixed', 
+    color: 'black',
+    fontSize: '4vw',
+    fontWeight: 'bold',
+    textAlign: 'left',
+    maxWidth: '70%',
+    margin: '3% 3%'
+  };
+  
+  const searchBarStyle = {
+    position: 'fixed', 
+    width: '80%',
+    top: '35%',
+    left: '5%',
+    margin: '3% 3%',
+    maxWidth: '700px',
+    borderRadius: '20px',
+    padding: '10px',
+    backgroundColor: 'white',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    fontSize: '1rem',
+    border: 'none',
+    outline:'none'
+ 
+  };
+  
+
+  
   return (
-    <div className = "background" style={backgroundStyle}>
-      <div className="App">
-        <header className="App-header">
-          {/* content goes here */}
-         
-        </header>
+    <div className="App" style={gradientBackground}>
+      <div style={titleStyle}>
+        Find a Cooling Center<br/>Near You
       </div>
+      
+      <input type="text" placeholder="Search..." style={searchBarStyle} />
+      
+      <WaveRibbon /> {/* Include the WaveRibbon component */}
+      
+      {/* more stuff??? */}
+
+     
     </div>
   );
 }
 
 export default App;
-
---
+++ src/WaveRibbon.js
@@ -0,0 +1,12 @@
+import './WaveRibbon.css'; // Import the CSS file
+
+const WaveRibbon = () => {
+  return (
+    <div className="ocean">
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  );
+}
+
+export default WaveRibbon;
